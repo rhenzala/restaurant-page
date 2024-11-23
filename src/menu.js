c5a698c48324509944da09b940ecf65ee57c1c9e
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-11-23 16:48:24 +0800

    Add image for each menu item
    Added image for better user experience so that the user
    can see the dish while browsing the menu.

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,7 @@
+import pizza from "/assets/pizza.jpeg";
+import shawarma from "/assets/shawarma.jpeg";
+import kebab from "/assets/kebab.jpeg";
+
 export function createMenuPage(){
     const content = document.getElementById('content');
 
@@ -14,37 +18,52 @@ export function createMenuPage(){
     const dishName1 = document.createElement('h2');
     const description1 = document.createElement('p');
     const price1 = document.createElement('p');
+    const dishImage1 = document.createElement('img');
     menu1.classList.add('menu', 'content-section');
-    dishName1.textContent = "Menu 1";
+    dishName1.textContent = "Pizza";
     description1.textContent = "Lorem ipsum odor amet, consectetuer adipiscing elit.";
     price1.textContent = "$2.99";
+    dishImage1.src = pizza;
+    dishImage1.alt = "Image of pizza";
+    dishImage1.classList.add('menu-image');
     menu1.appendChild(dishName1);
     menu1.appendChild(description1);
     menu1.appendChild(price1);
+    menu1.appendChild(dishImage1);
 
     const menu2 = document.createElement('div');
     const dishName2 = document.createElement('h2');
     const description2 = document.createElement('p');
     const price2 = document.createElement('p');
+    const dishImage2 = document.createElement('img');
     menu2.classList.add('menu', 'content-section');
-    dishName2.textContent = "Menu 2";
+    dishName2.textContent = "Shawarma";
     description2.textContent = "Lorem ipsum odor amet, consectetuer adipiscing elit.";
-    price2.textContent = "$3.99";
+    price2.textContent = "$1.99";
+    dishImage2.src = shawarma;
+    dishImage2.alt = "Image of Shawarma";
+    dishImage2.classList.add('menu-image');
     menu2.appendChild(dishName2);
     menu2.appendChild(description2);
     menu2.appendChild(price2);
+    menu2.appendChild(dishImage2);
 
     const menu3 = document.createElement('div');
     const dishName3 = document.createElement('h2');
     const description3 = document.createElement('p');
     const price3 = document.createElement('p');
+    const dishImage3 = document.createElement('img');
     menu3.classList.add('menu', 'content-section');
-    dishName3.textContent = "Menu 3";
+    dishName3.textContent = "Kebab";
     description3.textContent = "Lorem ipsum odor amet, consectetuer adipiscing elit.";
     price3.textContent = "$2.49";
+    dishImage3.src = kebab;
+    dishImage3.alt = "Image of Kebab";
+    dishImage3.classList.add('menu-image');
     menu3.appendChild(dishName3);
     menu3.appendChild(description3);
     menu3.appendChild(price3);
+    menu3.appendChild(dishImage3);
 
     content.appendChild(heading);
     content.appendChild(menu1);
